fc1f79731b90b8c939d1ecde643ed04a11259d72
Kadeem Pratt <10562319+Scoubershare@users.noreply.github.com>
2023-03-30 04:15:36 -1100

    build

--- jobport/src/component/projects.js
+++ jobport/src/component/projects.js
@@ -20,17 +20,25 @@ export default function Projects() {
       {showProjects && (
         <div className="dropdown-content">
           <p>
-            <a href="https://www.coursera.org/account/accomplishments/professional-cert/MNYMBRMC3ZDM">
-              IBM Data Science{" "}
-            </a>{" "}
-            <br /> Professional Certification <br />@ Coursera{" "}
-          </p>
-          <p>
-            <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/397G86RFMV5S">
-              Data Science Fundamentals with Python and SQL
+            <a href="https://github.com/Scoubershare/spaceX/blob/1051688b8d5c1a5484098cedaa44860113e77512/lab_jupyter_launch_site_location.ipynb">
+              SpaceX Launch Sites Analysis (Python){" "}
             </a>
           </p>
-
+          <p>
+            <a href="https://github.com/Scoubershare/DS-Stats/blob/5c7b634dd4d89bba5c93ef33ca2304b0388dfd84/Boston%20Housing%20Regression.ipynb">
+              Boston Housing Regeression Analysis (Python){" "}
+            </a>
+          </p>
+          <p>
+            <a href="https://github.com/Scoubershare/DS-Stats/blob/5c7b634dd4d89bba5c93ef33ca2304b0388dfd84/Boston%20Housing%20Regression.ipynb">
+              SpaceX Full Report (Powerpoint){" "}
+            </a>
+          </p>
+          <p>
+            <a href="https://github.com/Scoubershare/IBM-Data-Science/blob/5d6e8e6454e57767fdf61d05d1baab0336cd2c49/House_Sales_in_King_Count_USA%20(3).ipynb">
+             King County Housing Sales Data Analysis (Pyhton){" "}
+            </a>
+          </p>
         </div>
       )}
     </div>
